a3cda1f1ee8a4e7ae4511346ece2d70a266c2b89
routinues <routinues@gmail.com>
2023-02-09 23:51:13 +0300

    Hata Düzeltmeleri ve Performans İyileştirmeleri
    Index sayfasına navbar eklendi
    Ürün sayfası eklendi
    Ürün ekleme, silme ve güncelleme eklendi

--- Proje_OOP/Controllers/DefaultController.cs
+++ Proje_OOP/Controllers/DefaultController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Proje_OOP.Örnekler;
 
 namespace Proje_OOP.Controllers
 {
@@ -21,6 +22,44 @@ namespace Proje_OOP.Controllers
         public IActionResult Urunler()
         {
             mesajlar();
+            return View();
+        }
+
+        public IActionResult Deneme()
+        {
+            Sehirler sehirler = new Sehirler();
+
+            sehirler.Id = 1;
+            sehirler.Ulke = "Ukrayna";
+            sehirler.Sehir = "Kiev";
+            sehirler.Nufus = 1000000;
+            sehirler.Renk1 = "Mavi";
+            sehirler.Renk2 = "Sarı";
+
+            ViewBag.v1 = sehirler.Id;
+            ViewBag.v2 = sehirler.Ulke;
+            ViewBag.v3 = sehirler.Sehir;
+            ViewBag.v4 = sehirler.Nufus;
+            ViewBag.v5 = sehirler.Renk1;
+            ViewBag.v6 = sehirler.Renk2;
+            ViewBag.v7 = sehirler.Renk3;
+
+
+            sehirler.Id = 2;
+            sehirler.Ulke = "Makedeonya";
+            sehirler.Sehir = "Üsküp";
+            sehirler.Nufus = 5000000;
+            sehirler.Renk1 = "Sarı";
+            sehirler.Renk2 = "Kırmızı";
+
+            ViewBag.z1 = sehirler.Id;
+            ViewBag.z2 = sehirler.Ulke;
+            ViewBag.z3 = sehirler.Sehir;
+            ViewBag.z4 = sehirler.Nufus;
+            ViewBag.z5 = sehirler.Renk1;
+            ViewBag.z6 = sehirler.Renk2;
+            ViewBag.z7 = sehirler.Renk3;
+
             return View();
         }
     }
---
+++ Proje_OOP/Örnekler/Sehirler.cs
@@ -0,0 +1,10 @@
+﻿namespace Proje_OOP.Örnekler
+{
+    public class Sehirler : Bayrak
+    {
+        public int Id { get; set; }
+        public string Ulke { get; set; }
+        public string Sehir { get; set; }
+        public int Nufus { get; set; }
+    }
+}
